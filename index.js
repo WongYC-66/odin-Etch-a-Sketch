be1f6b9596682a0dbfc96c6a041f9bba6a0bad96
WongYC-66 <yengchien@hotmail.com>
2023-07-24 02:58:16 +0800

    Add in normal color mode, rainbow mode

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+let colorMode = 'normal'
+
 document.querySelector('#btnSize').addEventListener('click', e => {
     updateGrid(prompt('size = ? e.g. 1 ~ 100'));
+})
+document.querySelector('#btnNormalColor').addEventListener('click', e => {
+    colorMode = 'normal'
+})
+document.querySelector('#btnRainbow').addEventListener('click', e => {
+    colorMode = 'rainbow'
 })
 
 
@@ -12,13 +20,11 @@ function updateGrid(size){
     }
 
     // Add cell
-    console.log(container)
     const MARGINSIZE = 1;
     let totalWidth = container.clientWidth ;
     let totalHeight = container.clientHeight;
     let cellWidth = (totalWidth / size) - MARGINSIZE * 2
     let cellHeight = (totalHeight / size) - MARGINSIZE * 2
-    console.log(cellWidth, cellHeight)
 
     for(let i = 0 ; i < size; i++){
         let newRow = document.createElement('div')
@@ -29,8 +35,23 @@ function updateGrid(size){
             newCell.classList.add('cell')
             newCell.style.width = `${cellWidth}px`;
             newCell.style.height = `${cellHeight}px`;
+            newCell.style.backgroundColor = `rgb(255,255,255)`
             newCell.addEventListener("mouseenter" , e => {
-                e.target.style.backgroundColor = 'lightblue' ;
+                if(colorMode === 'rainbow'){
+                    let r = Math.floor(Math.random()*256)
+                    let g = Math.floor(Math.random()*256)
+                    let b = Math.floor(Math.random()*256) // random rainbow color
+                    e.target.style.backgroundColor = `rgb(${r},${g},${b})` ;
+                } else {
+                    // normal color mode : slowly from white > grey > black
+                    let rgbArr = e.target.style.backgroundColor.replaceAll(/[rgb\(\) ]/g, '').split(',')
+                    let r = parseInt(rgbArr[0]) - 26 
+                    let g = parseInt(rgbArr[1]) - 26 
+                    let b = parseInt(rgbArr[2]) - 26  // color darkens by 10% each interaction
+                    console.log(rgbArr)
+                    e.target.style.backgroundColor = `rgb(${r},${g},${b})` ;
+                }
+                
             })
             newRow.appendChild(newCell)
         }
@@ -39,5 +60,5 @@ function updateGrid(size){
 }
 
 // when refresh page:
-updateGrid(prompt('size = ? e.g. 1 ~ 100'));
+updateGrid(16);
     
